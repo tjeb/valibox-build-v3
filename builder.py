834415fbc30920a99563bf2a7d9e581df0190df3
Jelte Jansen <jelte.jansen@sidn.nl>
2018-01-19 16:44:59 +0100

    fix for pkg feed dir

--- builder.py
+++ builder.py
@@ -133,7 +133,7 @@ class Builder:
                         ))
             steps.append(CmdStep("git pull", "lede-source"))
 
-        sidn_pkg_feed_dir = "sidn_openwrt_pkgs"
+        sidn_pkg_feed_dir = "../sidn_openwrt_pkgs"
         if self.config.getboolean("sidn_openwrt_pkgs", "update_git"):
             steps.append(CmdStep("git clone https://github.com/SIDN/sidn_openwrt_pkgs %s" % sidn_pkg_feed_dir,
                                  conditional=DirExistsConditional('sidn_openwrt_pkgs')
@@ -165,7 +165,7 @@ class Builder:
 
             # Set that in the pkg feed data; we do not want to change the repository, so we make a copy and update that
             orig_sidn_pkg_feed_dir = sidn_pkg_feed_dir
-            sidn_pkg_feed_dir = sidn_pkg_feed_dir + "_local"
+            sidn_pkg_feed_dir = "sidn_openwrt_pkgs_local"
             steps.append(CmdStep("cp -r %s %s" % (orig_sidn_pkg_feed_dir, sidn_pkg_feed_dir)))
             steps.append(UpdatePkgMakefile(sidn_pkg_feed_dir, "spin/Makefile", "/tmp/spin-0.6-beta.tar.gz"))
 
