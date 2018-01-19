39628d2778f648d8de8b4654a2fc7b52bef62d6c
Jelte Jansen <jelte.jansen@sidn.nl>
2018-01-19 15:56:20 +0100

    remove now unnecessary function

--- valibox_builder/util.py
+++ valibox_builder/util.py
@@ -31,25 +31,3 @@ def basic_cmd_output(cmd):
     p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
     (stdout, _) = p.communicate()
     return stdout.decode("utf-8")
-
-def _find_getch():
-    try:
-        import termios
-    except ImportError:
-        # Non-POSIX. Return msvcrt's (Windows') getch.
-        import msvcrt
-        return msvcrt.getch
-
-    # POSIX system. Create and return a getch that manipulates the tty.
-    import sys, tty
-    def _getch():
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(fd)
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-    return _getch
